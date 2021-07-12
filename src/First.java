71cd8f82457369c435fb7cbb0da2ad882ca0f473
mdsab <mdsab@LAPTOP-9IUMOQI7>
2021-07-12 16:16:52 -0400

    Stay safe line added

--- src/First.java
+++ src/First.java
@@ -4,6 +4,7 @@ public class First {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Welcome to KFC");
+System.out.println("Stay safe");
 	}
 
 }
