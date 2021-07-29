67bcbc9fd746851d996306ed55825adeaa6f170e
mdsab <mdsab@LAPTOP-9IUMOQI7>
2021-07-29 15:55:10 -0400

    sale commit

--- src/First.java
+++ src/First.java
@@ -8,6 +8,7 @@ System.out.println("Stay safe");
 System.out.println("I am test KFC PROJECT on Jenkins ");
 System.out.println("Practice for khan");
 System.out.println("kfc sabeel 5 commit");
+System.out.println("kfc is starting sale 40%");
 	}
 
 }
