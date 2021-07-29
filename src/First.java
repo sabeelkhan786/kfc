43fc78b9e938dc7a26323e10fc31d9df4c4683bb
mdsab <mdsab@LAPTOP-9IUMOQI7>
2021-07-29 15:52:53 -0400

    5 commit

--- src/First.java
+++ src/First.java
@@ -6,6 +6,8 @@ public class First {
 System.out.println("Welcome to KFC");
 System.out.println("Stay safe");
 System.out.println("I am test KFC PROJECT on Jenkins ");
+System.out.println("Practice for khan");
+System.out.println("kfc sabeel 5 commit");
 	}
 
 }
